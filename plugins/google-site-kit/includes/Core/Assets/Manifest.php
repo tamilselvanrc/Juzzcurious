668b97ddb79aafc7456328f416a3f9902d3afd37
tamilselvanrc <tamilselvanrc@gmail.com>
2022-06-27 23:30:50 +0530

    first commit

---
+++ plugins/google-site-kit/includes/Core/Assets/Manifest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Class Google\Site_Kit\Core\Assets\Manifest
+ *
+ * @package   GoogleSite_Kit
+ * @copyright 2020 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Core\Assets;
+
+/**
+ * Assets manifest.
+ *
+ * @since 1.15.0
+ * @access private
+ * @ignore
+ */
+class Manifest {
+
+	public static $assets = array(
+		"googlesitekit-vendor"                     => "googlesitekit-vendor.a5445c17764433b62c6b.js",
+		"googlesitekit-activation"                 => "googlesitekit-activation.2606aa75d92ddc0ade9b.js",
+		"googlesitekit-adminbar"                   => "googlesitekit-adminbar.987cd92c3d0922764c77.js",
+		"googlesitekit-api"                        => "googlesitekit-api.09ab9760b5de26d868d2.js",
+		"googlesitekit-base"                       => "googlesitekit-base.b9c8f967b167ebe498a5.js",
+		"googlesitekit-dashboard"                  => "googlesitekit-dashboard.edd9be1e64c9e4e2e635.js",
+		"googlesitekit-dashboard-details"          => "googlesitekit-dashboard-details.202cdadcf22ad30c3cf8.js",
+		"googlesitekit-dashboard-splash"           => "googlesitekit-dashboard-splash.e5c9ef2ae2f6a0806b20.js",
+		"googlesitekit-data"                       => "googlesitekit-data.ef39e37cd9c5787ada02.js",
+		"googlesitekit-datastore-forms"            => "googlesitekit-datastore-forms.8ba84e166b0c0ee89cc6.js",
+		"googlesitekit-datastore-site"             => "googlesitekit-datastore-site.2800185b09c32d760f29.js",
+		"googlesitekit-datastore-user"             => "googlesitekit-datastore-user.11d4c5828c9e65be2b6c.js",
+		"googlesitekit-module"                     => "googlesitekit-module.30514234a9814f788551.js",
+		"googlesitekit-modules"                    => "googlesitekit-modules.4b5927f7fa4c4ef48068.js",
+		"googlesitekit-modules-adsense"            => "googlesitekit-modules-adsense.196154ecd208491e97b7.js",
+		"googlesitekit-modules-analytics"          => "googlesitekit-modules-analytics.00374cec486a0e34d133.js",
+		"googlesitekit-modules-optimize"           => "googlesitekit-modules-optimize.58b7d4e5e3c058aaf908.js",
+		"googlesitekit-modules-pagespeed-insights" => "googlesitekit-modules-pagespeed-insights.2dd42c634507f7659a4c.js",
+		"googlesitekit-modules-search-console"     => "googlesitekit-modules-search-console.cf2268ed20d3d511b12d.js",
+		"googlesitekit-modules-tagmanager"         => "googlesitekit-modules-tagmanager.3a35e782f05c4fc21dbb.js",
+		"googlesitekit-settings"                   => "googlesitekit-settings.129533b3c488fa793f62.js",
+		"googlesitekit-user-input"                 => "googlesitekit-user-input.2664ec162a30a42dd23e.js",
+		"googlesitekit-widgets"                    => "googlesitekit-widgets.f8a229e464b599d1e207.js",
+		"googlesitekit-wp-dashboard"               => "googlesitekit-wp-dashboard.f583813b71092c09ed18.js",
+		"pagead2.ads"                              => "pagead2.ads.5afac56536622708c33f.js",
+	);
+
+}
